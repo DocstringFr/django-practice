6aa703de8c203767dbd3fce9cd799631f45f0642
Thibault <houdon.thibault@gmail.com>
2021-03-30 22:17:22 +0200

    Add solution

--- src/practice/urls.py
+++ src/practice/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from store.views import books, book
+
 urlpatterns = [
+    path('', books),
+    path('<int:book_pk>/', book),
     path('admin/', admin.site.urls),
 ]
